99c4b75bf6cae69010262a12e4021eeeda79c05f
Spanaic <spanaic@gmail.com>
2020-07-24 13:06:27 +0900

    途中コミット

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +14,15 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// import Vue from "vue/dist/vue.esm";
+import Vue from "vue";
+import App from "../packs/app.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    el: "#app",
+    data: {},
+    components: { App },
+  });
+});
